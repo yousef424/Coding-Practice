f1ded5802a54e227cb1170b991f99671d5b7f960
Yousef Shehata <yousef424ae@yahoo.com>
2021-08-04 03:08:14 +0400

    Add files via upload

---
+++ Exception Handling/Exception Handling/bin/Debug/netcoreapp3.1/Exception Handling/Exception Handling/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Exception_Handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool repeat = false;
+            try
+            {
+                while (!repeat)
+                {
+
+
+                    Console.Write(" enter a number: ");
+                    int num1 = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write(" enter a number: ");
+                    int num2 = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine(num1 / num2);
+                    Console.ReadLine();
+                    repeat = true;
+                }
+            }
+
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+                repeat = false;
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+
+            Console.ReadLine();
+           
+
+        }
+    }
+}
