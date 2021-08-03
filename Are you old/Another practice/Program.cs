58c42c567574e98911d920afbabd0f42a76057bc
Yousef Shehata <yousef424ae@yahoo.com>
2021-08-04 02:56:26 +0400

    Add files via upload

---
+++ Are you old/Another practice/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Another_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            if( n % 2 > 0)
+            {
+                Console.WriteLine("weird");
+            }
+            else if( n % 2 == 0 && n >=2 && n<=5)
+            {
+                Console.WriteLine("Not Weird");
+            }
+            else if (n % 2 == 0 && n >= 6 && n <= 20)
+            {
+                Console.WriteLine("weird");
+            }
+            else if (n % 2 == 0 && n > 20)
+            {
+                Console.WriteLine("weird");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Better calculator/Better calculator/Program.cs
@@ -0,0 +1,42 @@
+﻿ using System;
+
+namespace Better_calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter a number buddy: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("whatcha wanna do ");
+            string op = Console.ReadLine();
+
+            Console.Write("enter a number buddy: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            if(op == "+")
+            {
+                Console.WriteLine(num1 + num2);
+            }
+            else if (op == "-")
+            {
+                Console.WriteLine(num1 - num2);
+            }
+            else if (op == "*")
+            {
+                Console.WriteLine(num1 * num2);
+            }
+            else if (op == "/")
+            {
+                Console.WriteLine(num1 / num2);
+            }
+            else
+            {
+                Console.WriteLine("invalid operator");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
