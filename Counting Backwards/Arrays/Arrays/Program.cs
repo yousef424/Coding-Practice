cbee59e76fec2cf0272d728658e78a979843148c
Yousef Shehata <yousef424ae@yahoo.com>
2021-08-04 03:04:41 +0400

    Add files via upload

---
+++ Counting Backwards/Arrays/Arrays/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32( Console.ReadLine());
+            int[] nums = new int[n];
+
+            for(int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = int.Parse(Console.ReadLine());
+            }
+
+            for(int i = nums.Length-1; i >=0; i--)
+            {
+                Console.Write(nums[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
