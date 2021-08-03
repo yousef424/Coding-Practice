70ea38eb2a9c36a29b84fe5ac95d8a6a0cf13fc5
Yousef Shehata <yousef424ae@yahoo.com>
2021-08-04 02:55:28 +0400

    Add files via upload

---
+++ 2D Arrays/2D Arrays/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _2D_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] numberGrid =
+            {
+                {1,2},
+                {3,5},
+                {2,6}
+            };
+
+            int[,] myArray = new int[0, 0];
+            
+            Console.Write(numberGrid[1, 0]);
+            Console.ReadLine();
+        }
+    }
+}
