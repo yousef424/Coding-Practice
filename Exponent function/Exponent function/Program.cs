03dfe8ca56396be882e5b35c60ec17c0d73da7a4
Yousef Shehata <yousef424ae@yahoo.com>
2021-08-04 03:10:40 +0400

    Add files via upload

---
+++ Exponent function/Exponent function/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Exponent_function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetPow(3,3));
+            Console.ReadLine();
+        }
+
+        static int GetPow(int baseNum, int powNum)
+        {
+            int result = 1;
+
+            for(int i = 0; i<powNum; i++)
+            {
+                result = result * baseNum;
+            }
+
+
+
+
+
+            return result;
+        }
+    }
+}
---
+++ For loops/For loops/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace For_loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] luckyNumbers = { 4, 5, 6, 78, 98, 54 };
+
+            for(int i = 0; i<luckyNumbers.Length; i++)
+            {
+                Console.WriteLine(luckyNumbers[i]);
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
