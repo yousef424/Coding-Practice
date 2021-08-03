cedd2991dfbb437c5b6661415619bb2e3a9a2bad
Yousef Shehata <yousef424ae@yahoo.com>
2021-08-04 03:06:49 +0400

    Add files via upload

---
+++ Decision Making/Decision Making/Decision Making/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Decision_Making
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int row = 0; row < 10; row++)
+            {
+
+                
+
+
+                for (int spacing = 9 -row ; spacing > 0 ; spacing--)
+                {
+                   
+                    Console.Write(" ");
+                }
+                for (int column = 0; column < (2 * row + 1); column++)
+                {
+                   // Console.Write(" ");
+
+                    Console.Write("*");
+
+                }
+
+                Console.WriteLine();
+            
+
+
+
+            }
+                Console.ReadKey();
+
+        }
+    }
+}
