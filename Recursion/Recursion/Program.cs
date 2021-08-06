583c53ef443693746f3e6194eace371c05f189e0
Yousef Shehata <yousef424ae@yahoo.com>
2021-08-06 17:01:18 +0400

    Add files via upload

---
+++ Recursion/Recursion/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Factorial(3));
+            Console.ReadLine();
+
+        }
+
+        public static int Factorial(int n)
+        {
+            if(n <= 1 )
+            {
+                return n;
+            }
+            else
+            {
+                return n * Factorial(n - 1);
+            }    
+
+        }
+    }
+}
