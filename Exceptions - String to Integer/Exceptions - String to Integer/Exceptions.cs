a3256df958df5632a90e5db852a42a9a15af45b0
Yousef Shehata <yousef424ae@yahoo.com>
2021-08-20 13:21:14 +0400

    Add files via upload

---
+++ Exceptions - String to Integer/Exceptions - String to Integer/Exceptions.cs
@@ -0,0 +1,16 @@
+﻿using DotLiquid.Tags;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exceptions___String_to_Integer
+{
+    public class Exceptions
+    {
+
+        public static void PowerException()
+        {
+            throw new Exception("n and p should be non-negative");
+        }
+    }
+}
