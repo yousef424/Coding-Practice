4a58049f4086bbf50b98c81ab15bba43e4422b23
Yousef Shehata <yousef424ae@yahoo.com>
2021-08-22 15:06:52 +0400

    Add files via upload

---
+++ AdvancedArithmetic/AdvancedArithmetic/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AdvancedArithmetic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+           
+            for(int i =1; i<=n; i++)
+            {
+                if( n % i ==0)
+                {
+                  sum +=  n / i;
+                }
+
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
